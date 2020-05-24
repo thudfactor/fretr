f3d73819dfed802a83f5f311bf6b0cf29cbd4637
John Williams <john@insidenewcity.com>
2020-05-24 12:16:12 -0400

    Note to Midi implementation, also tests

--- src/utils/notation.test.ts
+++ src/utils/notation.test.ts
@@ -2,41 +2,39 @@ import { noteToMidi, midiToNote } from "./notation";
 
 test("returns middle C", () => {
   const middleC = midiToNote(60);
-  expect(middleC.note).toBe("C");
+  expect(middleC.name).toBe("C");
   expect(middleC.octave).toBe(4);
 });
 
 test("returns middle C with flats", () => {
-  const { note, octave } = midiToNote(60, { flats: true });
-  expect(note).toBe("C");
+  const { name, octave } = midiToNote(60, { flats: true });
+  expect(name).toBe("C");
   expect(octave).toBe(4);
 });
 
-test("returns F#3", () => {
-  const { note, octave } = midiToNote(54);
-  expect(note).toBe("F♯");
+test("returns F♯3", () => {
+  const { name, octave } = midiToNote(54);
+  expect(name).toBe("F♯");
   expect(octave).toBe(3);
 });
 
-test("returns Gb3", () => {
-  const { note, octave } = midiToNote(54, { flats: true });
-  expect(note).toBe("G♭");
+test("returns G♭3", () => {
+  const { name, octave } = midiToNote(54, { flats: true });
+  expect(name).toBe("G♭");
   expect(octave).toBe(3);
 });
 
 test("returns lower bound", () => {
-  const { note, octave } = midiToNote(21);
-  expect(note).toBe("A");
+  const { name, octave } = midiToNote(21);
+  expect(name).toBe("A");
   expect(octave).toBe(0);
 });
 
 test("returns upper bound", () => {
-  const highestC = midiToNote(108);
-  expect(highestC.note).toBe("C");
-  expect(highestC.octave).toBe(8);
+  const { name, octave } = midiToNote(108);
+  expect(name).toBe("C");
+  expect(octave).toBe(8);
 });
-
-test("throws on low values", () => {});
 
 test("throws on high values", () => {
   expect(() => {
@@ -47,5 +45,65 @@ test("throws on high values", () => {
 test("throws on high values", () => {
   expect(() => {
     midiToNote(109);
+  }).toThrow();
+});
+
+test("Returns 60 for middle C", () => {
+  const noteID = noteToMidi({ name: "C", octave: 4 });
+  expect(noteID).toBe(60);
+});
+
+test("Returns 108 for C8", () => {
+  const noteID = noteToMidi({ name: "C", octave: 8 });
+  expect(noteID).toBe(108);
+});
+
+test("Returns 21 for A0", () => {
+  const noteID = noteToMidi({ name: "A", octave: 0 });
+  expect(noteID).toBe(21);
+});
+
+test("Returns 32 for G♯", () => {
+  const noteID = noteToMidi({ name: "G♯", octave: 1 });
+  expect(noteID).toBe(32);
+});
+
+test("Returns 32 for A♭", () => {
+  const noteID = noteToMidi({ name: "A♭", octave: 1 });
+  expect(noteID).toBe(32);
+});
+
+test("Returns 32 for A♭", () => {
+  const noteID = noteToMidi({ name: "A♭", octave: 1 });
+  expect(noteID).toBe(32);
+});
+
+test("throws on nonexistant notes", () => {
+  expect(() => {
+    noteToMidi({ name: "C♭", octave: 1 });
+  }).toThrow();
+});
+
+test("throws on super low notes", () => {
+  expect(() => {
+    noteToMidi({ name: "C", octave: 0 });
+  }).toThrow();
+});
+
+test("throws on super high notes", () => {
+  expect(() => {
+    noteToMidi({ name: "A", octave: 8 });
+  }).toThrow();
+});
+
+test("throws on low octaves", () => {
+  expect(() => {
+    noteToMidi({ name: "A", octave: -1 });
+  }).toThrow();
+});
+
+test("throws on high octaves", () => {
+  expect(() => {
+    noteToMidi({ name: "A", octave: 10 });
   }).toThrow();
 });
--- src/utils/notation.ts
+++ src/utils/notation.ts
@@ -1,16 +1,40 @@
 import sharpNotes from "./sharpNotes.json";
 import flatNotes from "./flatNotes.json";
 
-export function noteToMidi(note: string, octave: number) {}
+interface NoteInterface {
+  name: string;
+  octave: number;
+}
 
-export function midiToNote(midiNumber: number, options = { flats: false }) {
+export function noteToMidi(note: NoteInterface): number {
+  const { name, octave } = note;
+  if (octave < 0 || octave > 8) throw Error(`Octave out of range, ${octave}`);
+
+  let noteIndex = sharpNotes.indexOf(name);
+  if (noteIndex < 0) noteIndex = flatNotes.indexOf(name);
+  if (noteIndex < 0) {
+    throw Error(`Not an actual note, ${name}, ${noteIndex}`);
+  }
+
+  // Limit to piano notes, A0 to C8
+  if ((octave === 0 && noteIndex < 9) || (octave === 8 && noteIndex > 0)) {
+    throw Error(`Note out of range, ${name}${octave}`);
+  }
+
+  return 12 + noteIndex + octave * 12;
+}
+
+export function midiToNote(
+  midiNumber: number,
+  options = { flats: false }
+): NoteInterface {
   const { flats } = options;
 
-  if (midiNumber < 21) throw Error("Note number out of range " + midiNumber);
-  if (midiNumber > 108) throw Error("Note number out of range " + midiNumber);
+  if (midiNumber < 21 || midiNumber > 108)
+    throw Error("Note number out of range " + midiNumber);
 
   const octave = Math.floor(midiNumber / 12) - 1;
-  const note = flats ? flatNotes[midiNumber % 12] : sharpNotes[midiNumber % 12];
+  const name = flats ? flatNotes[midiNumber % 12] : sharpNotes[midiNumber % 12];
 
-  return { note, octave };
+  return { name, octave };
 }
