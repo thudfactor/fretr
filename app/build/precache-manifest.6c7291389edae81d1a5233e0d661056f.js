45c822c51e2d840b81be4741eaa09c992651ade9
John Williams <john@insidenewcity.com>
2020-09-17 13:02:53 -0400

    Reorgainize directory structure, add docker workflow.

---
+++ app/build/precache-manifest.6c7291389edae81d1a5233e0d661056f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c28bed0f28efff44567c7925867e276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6af3bd6b3e427f18aa22",
+    "url": "/static/css/main.ba3e91d2.chunk.css"
+  },
+  {
+    "revision": "8248262ff154c0375d4a",
+    "url": "/static/js/2.db6e05b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.db6e05b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af3bd6b3e427f18aa22",
+    "url": "/static/js/main.fddbce1d.chunk.js"
+  },
+  {
+    "revision": "1381f339e53703be43f5",
+    "url": "/static/js/runtime-main.cea1a1f6.js"
+  }
+]);
